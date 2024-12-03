88ffdda3bb38101ad90103e9cb99b2401a358560
fasih-nasir <fasihnasir12@gmail.com>
2024-12-04 04:30:06 +0500

    Updated code with new changes

--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -1,10 +1,134 @@
-import React from 'react'
-import '../../App.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../App.css";
+import 'animate.css';
+
+// Import Images
+import car1 from "../image/car1.png";
+import car2 from "../image/car2.png";
+import car3 from "../image/car3.png";
 
 export default function Home() {
   return (
-    <div>
-      Home page
-    </div>
-  )
+ 
+      <div
+
+        id="carouselExampleFade"
+        className="container-fluid m-0 p-0 carousel slide carousel-fade"
+        data-bs-ride="carousel"
+      data-bs-interval="3500" // This will change the image every 2 seconds
+      >
+        <div className="carousel-inner m-0 p-0">
+          <div className="carousel-item active d-flex flex-column justify-content-center align-items-center">
+            <img
+              className="d-block w-100 h-100 position-relative"
+              src={car2}
+              alt="Car 1"
+            />
+            <div className="position-absolute d-flex flex-column col-lg-10 col-12 justify-content-center align-items-center">
+            <h1 className="carousel-fade animate__animated animate__fadeInDown    col-lg-7 col-10 text-center text-white text-capitalize display-5  fw-bold">
+              Helping businesses unlock their true potential with{" "}
+              <span className="org  px-2 py-0 rounded-3"> consultancy.</span>
+            </h1>
+           {/*  */}
+           <Link to={"/about"} className="text-decoration-none">
+       
+            <button className="bookmarkBtn mt-4">
+    <span className="IconContainer">
+     
+      <i class="fa-solid fa-arrow-right icon"></i>
+    </span>
+    <Link className="text col-8 text-decoration-none" to={"/about"}>More-Info</Link>
+    
+  </button>
+  </Link>
+  {/*  */}
+          </div>
+          </div>
+          <div className=" carousel-item d-flex justify-content-center align-items-center">
+            <img
+              className="d-block w-100 position-relative"
+              src={car1}
+              alt="Car 2"
+            />
+            <div className="position-absolute d-flex flex-column col-12  justify-content-center align-items-center">
+
+            <h1 className="carousel-fade animate__animated animate__fadeInDown col-12   col-lg-8 text-center text-white text-capitalize display-5 fw-bold">
+              Empowering organizations to grow through{" "}
+              <span className="org  px-2 py-0 rounded-3">strategic</span>{" "}
+              insights and innovative solutions.
+            </h1>
+            <Link to={"/about"} className="text-decoration-none">
+
+            <button className="bookmarkBtn mt-4">
+    <span className="IconContainer">
+     
+      <i class="fa-solid fa-arrow-right icon"></i>
+    </span>
+    <Link className="text col-8 text-decoration-none" to={"/about"}>More-Info</Link>
+    
+  </button>
+</Link>
+  {/*  */}
+
+            </div>
+         
+          </div>
+          <div className="carousel-item d-flex justify-content-center align-items-center">
+            <img
+              className="d-block w-100 position-relative"
+              src={car3}
+              alt="Car 3"
+            />
+            <div className="position-absolute d-flex flex-column col-12 justify-content-center align-items-center">
+
+            <h1 className="carousel-fade animate__animated animate__fadeInDown  col-lg-8 col-11 text-center text-white text-capitalize display-5 fw-bold">
+              Guiding leaders toward success with tailored solutions for a{" "}
+              <span className="org  px-2 py-0 rounded-3"> brighter</span>{" "}
+              future.
+            </h1>
+            <Link to={"/about"} className="text-decoration-none">
+            <button className="bookmarkBtn mt-4">
+    <span className="IconContainer">
+     
+      <i class="fa-solid fa-arrow-right icon"></i>
+    </span>
+    <Link className="text col-8 text-decoration-none" >More-Info</Link>
+    
+  </button>
+  </Link>
+  {/*  */}
+            </div>
+
+          </div>
+        </div>
+
+        {/* Carousel controls */}
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          {/* <span className="visually-hidden">Next</span> */}
+        </button>
+      </div>
+    
+  );
 }
--- src/assets/pages/navbar.jsx
+++ src/assets/pages/navbar.jsx
@@ -2,47 +2,60 @@
 import '../../App.css'
 // 
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState,useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";
 // 
 // IMAGES
 import logodark from "../image/logo-dark.png"
 // IMAGES
 
 export default function Navbar() {
+  // ACTIVE
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
+
+  useEffect(() => {
+    setActivePath(location.pathname); // Update active path on location change
+  }, [location]);
+  // ACTIVE
+  // STICKE NAVBAR
+  window.addEventListener("scroll",function(){
+    var navbar=document.getElementById('navbar')
+if(window.scrollY > 500){
+  navbar.classList.add("fixtop")
+}
+else{
+  navbar.classList.remove("fixtop")
+}
+})
+
+
+  // STICKY NAVBAR
   return (
     <div>
         {/* TOP HEADER */}
-<div className="container-fluid d-flex flex-row justify-content-center align-items-center tophead m-0 px-4 bg-white py-3">
-   <div className="col-3 d-flex  align-items-center">
-<a href="#">
-<img src={logodark} className='img-fluid col-7' alt="" />
-</a>
-   </div>
-   <div className="col-4 d-flex  align-items-center">
-   <i class="fa-solid span-head fa-location-dot pe-2"></i>
+<div className="container-fluid d-flex flex-row d-lg-flex d-none justify-content-center align-items-center tophead m-0 px-4 bg-white py-3">
+
+   <div className="col-6 d-flex  align-items-center">
+   <i className="fa-solid span-head fa-location-dot pe-2"></i>
     <span className='span-head'>76-C Second Floor Street 2 DHA,  Karachi, Pakistan</span>
    </div>
-   <div className="col-2 d-flex  align-items-center">
-   <i class="fa-solid fa-phone pe-2 span-head"></i>
+   <div className="col-6 d-flex justify-content-end  align-items-center">
+   <i className="fa-solid fa-phone pe-2 span-head"></i>
     <span className='span-head'>
     +92 336 3170881
     </span>
    </div>
-   <div className="col-3 d-flex justify-content-end align-items-center">
-    <span className='mx-2 px-3 py-2  bg-d'><i class="fa-brands fa-facebook-f   "></i></span>
-    <span className='mx-2 px-3 py-2 bg-d'><i class="fa-brands fa-linkedin-in "></i></span>
-    <span className='mx-2 px-3 py-2 bg-d'><i class="fa-brands fa-instagram "></i></span>
-    {/* <span><i class="fa-brands fa-facebook-f"></i></span> */}
-
-   </div>
+   
+ 
 </div>
         {/* TOP HEADER */}
 
     {/*NAVBAR  */}
-    <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
+    <nav className="navbar navbar-expand-lg  px-3" id="navbar">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      Navbar
+    <a className="navbar-brand col-lg-2 col-4" href="#">
+      <img src={logodark} className='col-12' alt="" />
     </a>
     <button
       className="navbar-toggler"
@@ -56,69 +69,59 @@ export default function Navbar() {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            Link
-          </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a
-            className="nav-link disabled"
-            href="#"
-            tabIndex={-1}
-            aria-disabled="true"
-          >
-            Disabled
-          </a>
-        </li>
-      </ul>
-      <form className="d-flex">
+    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+      <li className="nav-item px-2">
+        <Link
+          to="/"
+          className={`nav-link ${activePath === "/" ? "active" : ""}`}
+          aria-current="page"
+        >
+          Home
+        </Link>
+      </li>
+      <li className="nav-item px-2">
+        <Link
+          to="/about"
+          className={`nav-link ${activePath === "/about" ? "active" : ""}`}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item px-2">
+        <Link
+          to="/service"
+          className={`nav-link ${activePath === "/service" ? "active" : ""}`}
+        >
+          Service
+        </Link>
+      </li>
+      <li className="nav-item px-2">
+        <Link
+          to="/contact"
+          className={`nav-link ${activePath === "/contact" ? "active" : ""}`}
+        >
+          Contact
+        </Link>
+      </li>
+      <li className="nav-item px-2">
+        <Link
+          to="/job"
+          className={`nav-link ${activePath === "/job" ? "active" : ""}`}
+        >
+          Jobs
+        </Link>
+      </li>
+    </ul>
+      <form className="d-flex flex-row justify-content-center col-3">
         <input
           className="form-control me-2"
           type="search"
           placeholder="Search"
           aria-label="Search"
         />
-        <button className="btn btn-outline-success" type="submit">
-          Search
+        <button className="btn p-2 ms-2" type="submit">
+          
+<i className="fa fa-search" aria-hidden="true"></i>
         </button>
       </form>
     </div>
@@ -126,6 +129,9 @@ export default function Navbar() {
 </nav>
 
     {/* NAVBAR */}
+
+
+
     </div>
   )
 }
