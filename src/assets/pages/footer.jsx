6b944f7f296122c4e19c2f41462344819f75f167
fasih-nasir <fasihnasir12@gmail.com>
2024-12-17 02:46:15 +0500

    Updated code with new changes

--- src/assets/pages/footer.jsx
+++ src/assets/pages/footer.jsx
@@ -22,13 +22,13 @@ const Footer = () => {
       <div className="container-fluid px-4">
         {/* Logo and Copyright */}
         <div className="d-flex flex-lg-row flex-column justify-content-between align-items-center mb-3">
-          <div className="d-flex align-items-center col-lg-6 col-12">
-            <img src={logodark} alt="Emphires"  className="me-lg-2 col-lg-4 col-10 mb-lg-0 mb-2 mx-auto" />
+          <div className="d-flex align-items-center align-items-lg-start justify-content-start col-lg-6 col-12">
+            <img src={logodark} alt="Emphires"  className="me-lg-2 col-lg-4 col-10 mb-lg-0 mb-2 mx-lg-0 mx-auto " />
             
           </div>
-          <div className="d-flex fle-lg-row flex-column col-lg-5 ms-1 justify-content-end gap-2 mt-lg-0 mt-4">
-          <input type="email" id="email" className="col-12 mx-auto px-2 rounded-3 border-0" placeholder=" Enter email address" />
-            <button className="btn bg-white col-lg-2">Send</button>
+          <div className="d-flex fle-lg-row flex-column col-lg-6 ms-1 justify-content-center justify-content-lg-end align-items-end gap-2 mt-lg-0 mt-4">
+          <input type="email" id="email" className="col-12 col-lg-8  mx-lg-0  px-2 rounded-3 border-0" placeholder=" Enter email address" />
+            <button className="btn bg-white col-12 col-lg-3">Send</button>
          
             {/* <small>Copyright © 2021 Empires. All Rights Reserved.</small> */}
           </div>
